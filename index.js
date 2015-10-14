8c62cd5a8a9cd9bb7dd22f67117d79c6da9a967d
Charles Perrone <chperrone@gmail.com>
2015-10-14 14:01:52 -0400

    integrated express server

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
+app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+app.use("/public", express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (req, res) {
+  res.sendFile(app.get('views') + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/contact', function (req, res) {
+  res.sendFile(app.get('views') + '/contact.html');
 });
 
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
+  console.log('Example app listening at http://%s:%s', host, port);
+});
