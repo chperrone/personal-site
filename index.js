7b1eb92b89e54b2c02bc1f8f815a8c32a6460fff
Charles Perrone <chperrone@gmail.com>
2015-10-16 10:33:51 -0400

    link projects page

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var app = express();
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
+
 app.use("/public", express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,6 +16,10 @@ app.get('/', function (req, res) {
 app.get('/contact', function (req, res) {
   res.sendFile(app.get('views') + '/contact.html');
 });
+
+app.get('/projects', function (req, res) {
+  res.sendFile(app.get('views') + '/projects.html');
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
